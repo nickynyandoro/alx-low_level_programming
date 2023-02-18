05718c7962075e41f65990862f5ba0de12508bf7
nickynyandoro <mutsawashenyandoro@gmail.com>
2023-02-18 03:13:11 -0800

    I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 /**
- * main -> assign a random number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable n
- * Return: Always 0 (Success)
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return:Always 0.
  */
 int main(void)
 {
-int n;
+	char letter;
 
-for (n = 0; n < 10; n++)
-{
-printf("%d", n);
-}
-printf("\n");
-return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
