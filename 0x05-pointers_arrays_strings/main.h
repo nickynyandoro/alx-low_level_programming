a87c9331b4f5f8ef3e0a7ac31d4718227f9b115a
nickynyandoro <mutsawashenyandoro@gmail.com>
2023-02-27 00:16:25 -0800

    A good engineer

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a int n);
+char *_strcpy(char *dest, char *src);
+
+
+#endif
+/*
+ * Nicola
+ */
