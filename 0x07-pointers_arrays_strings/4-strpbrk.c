11f0b4d0a4d39eb2a26d45ac379eb3a1f80c645c
nickynyandoro <mutsawashenyandoro@gmail.com>
2023-03-06 08:03:10 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strbrk - function that searches a string for any of a set of bytes
+ * _strpbrk - function that searches a string for any of a set of bytes
  *
  * @s: first occurence in the string
  *
