c82a655ffd5626d338ef62a637feb3f15eb9577a
nickynyandoro <mutsawashenyandoro@gmail.com>
2023-03-08 13:50:00 -0800

    She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a line
+ * @s: string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursions(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
+}
