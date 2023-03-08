94b5ccdbb6e69820463cf3e1f9e91fc2ea6dd377
nickynyandoro <mutsawashenyandoro@gmail.com>
2023-03-08 14:34:10 -0800

    She locked away a secret

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,9 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-	_putchar(*s);
-	_puts_recursions(s + 1);
+		_putchar(*s);
+		_puts_recursions(s + 1);
 	}
 	else
-	{
-	_putchar('\n');
-	}
-
+		_putchar('\n');
 }
